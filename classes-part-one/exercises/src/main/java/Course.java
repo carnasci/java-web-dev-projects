9134005d27f80781e65e560176c3f200d9674c9c
Carlos Nascimento <cen.master24@gmail.com>
2023-12-07 22:14:33 -0600

    classes part one exercises finished

--- classes-part-one/exercises/src/main/java/Course.java
+++ classes-part-one/exercises/src/main/java/Course.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Course {
+import java.util.ArrayList;
+
+public class Course {
+    private String subject;
+    private String instructor;
+    private ArrayList<Student> enrolledStudents;
+    public Course(String subject, String instructor, ArrayList<Student> enrolledStudents){
+        this.subject = subject;
+        this.instructor = instructor;
+        this.enrolledStudents = enrolledStudents;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    public ArrayList<Student> getEnrolledStudents() {
+        return enrolledStudents;
+    }
+
+    public void setEnrolledStudents(ArrayList<Student> enrolledStudents) {
+        this.enrolledStudents = enrolledStudents;
+    }
 }
--- classes-part-one/exercises/src/main/java/Student.java
+++ classes-part-one/exercises/src/main/java/Student.java
@@ -4,7 +4,38 @@ public class Student {
         private int numberOfCredits = 0;
         private double gpa = 0.0;
 
+        public Student(String name, int studentId, int numberOfCredits, double gpa) {
+                this.name = name;
+                this.studentId = studentId;
+                this.numberOfCredits = numberOfCredits;
+                this.gpa = gpa;
+        }
+
         // Drop your getters and setters below for the Student class.
         // To instantiate the Student class, add your code to the main in the file, SchoolPractice.
+        public String getName() {
+                return name;
+        };
+        public void setName(String aName) {
+                name = aName;
+        };
+        public int getStudentId() {
+                return studentId;
+        };
+        public void setStudentId(Integer aStudentId) {
+                studentId = aStudentId;
+        };
+        public int getNumberOfCredits() {
+                return numberOfCredits;
+        };
+        public void setNumberOfCredits(Integer aNumberOfCredits) {
+                numberOfCredits = aNumberOfCredits;
+        };
+        public double getGpa() {
+                return gpa;
+        };
+        public void setGpa(Double aGpa) {
+                gpa = aGpa;
+        };
 
 }
