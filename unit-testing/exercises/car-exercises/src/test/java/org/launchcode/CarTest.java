508c704e52a0ac6533ac22869c9c4605494a76cd
carnasci <cen.master24@gmail.com>
2024-01-05 18:26:07 -0600

    finished unit-testing exercises

--- unit-testing/exercises/car-exercises/src/test/java/org/launchcode/CarTest.java
+++ unit-testing/exercises/car-exercises/src/test/java/org/launchcode/CarTest.java
@@ -25,6 +25,24 @@ class CarTest {
         assertEquals( 10, test_car.getGasTankLevel(),.001);
     }
 
+    @Test
+    public void testGasTankAfterDriving() {
+        test_car.drive(50);
+        assertEquals(9, test_car.getGasTankLevel(), .001);
+    }
+
+    @Test
+    public void testGasTankAfterDrivingPastTankRange() {
+        test_car.drive(666);
+        assertEquals(test_car.getGasTankLevel(), 0, .001);
+    }
+
+    @Test
+    public void testGasTankOverfillException() {
+        assertThrows(IllegalArgumentException.class, () -> test_car.addGas(5), "Shouldn't get here, cannot add more" +
+                "gas than the tank allows.");
+    }
+
     //TODO: gasTankLevel is accurate after driving within tank range
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
     //TODO: can't have more gas than tank size, expect an exception
